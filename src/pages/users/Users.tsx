137ee43881fb31b037f83827a5ce646215d0b36c
Claudio Olivares <ca.olivares91@gmail.com>
2026-09-10 20:31:53 -0300

    feat: lint mejora

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import { UsersHeader } from "../../components/user/UserHeader";
-import type { User } from "../../shared/types/types";
 import { UserDrawer } from "./UserDrawer";
 
 export const Users = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const handleCreate = () => {
-    setSelectedUser(null);
     setDrawerOpen(true);
   };
   return (
